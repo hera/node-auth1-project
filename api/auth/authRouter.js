642ce5ecd18d10490d95342b2e8aa5a18d7a7281
hera <a.heraimenka@gmail.com>
2020-07-20 12:30:13 -0700

    Add empty routes

---
+++ api/auth/authRouter.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+    res.status(501).send("Not implemented");
+});
+
+router.post("/login", (req, res) => {
+    res.status(501).send("Not implemented");
+});
+
+router.get("/logout", (req, res) => {
+    res.status(501).send("Not implemented");
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
+const authRouter = require("./api/auth/authRouter");
+const usersRouter = require("./api/users/usersRouter");
 
 const PORT = process.env.PORT || 5000;
 const server = express();
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
 
 server.listen(PORT, () => "Server is listening...");
